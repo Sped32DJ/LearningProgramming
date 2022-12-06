d020f2bd1cff1cd02077d24e011cec87d3a27f62
Sped32DJ <dannycool10236@gmail.com>
2022-12-06 01:25:08 -0800

    Day 2... I'll solve this tomorrow lol

---
+++ aoc/Day2/rps.py
@@ -0,0 +1,14 @@
+'''Rock Paper Scissors simulator'''
+# Rock  = "A"
+# Paper = "Y"
+# Scssr = "C"
+# input.txt
+#   Left is opponent, right is you
+A = 50
+Y = 20
+C = 10
+
+if A>C:
+    print("Rock Wins")
+else:
+    print("haha")

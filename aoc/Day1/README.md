0dd7fcf993a237ef4759f6f6225ac59942ba3eb0
Sped32DJ <dannycool10236@gmail.com>
2022-12-06 00:11:24 -0800

    Day 1 Complete!

---
+++ aoc/Day1/README.md
@@ -0,0 +1,15 @@
+## Solution
+# Why did I use bash, vim, python for this?
+* idk how to code lol
+# how did you get a solution?
+* curl https://adventofcode.com/2022/day/1/input >> input.txt
+vim, recorded macro (editing input.txt)
+: 0j bckspc +
+$ sed 's/++/\n/g' test.txt >> workable.py
+: This only works because there is double ++ for every white space XD
+
+---
+* 0i print ( esc A ) esc 0 j i
+> python3 workable.txt >> numbers.py
+* 0ji esc bckspc , space
+* Then, I wrote real code in numbers.py, and got my solution after running it!
--- aoc/Day1/day1.py
+++ aoc/Day1/day1.py
@@ -15,11 +15,17 @@ sum(Elfour)
 sum(Elfive)
 
 print("lol")
-# How I solved it lol
+print("ignore above, that is useless")
+print("The real resolution is down here :)")
+
+# How I solved it lol (Yes, I used vim macros/sed/python)
 # curl https://adventofcode.com/2022/day/1/input >> input.txt
 # vim, recorded macro (editing input.txt)
 #    0j bckspc +
 #$ sed 's/++/\n/g' test.txt >> workable.py
+# This only works because there is double ++ for every white space XD
+
 # 0i print ( esc A ) esc 0 j i
 #$ python3 workable.txt >> numbers.py
 # 0ji esc bckspc , space
+# Then, I wrote real code in numbers.py, and got my solution after running it!

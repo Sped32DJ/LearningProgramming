3c4490bbe9d91c76051de5e067e1574db2ff224a
BuildTools <unconfigured@null.spigotmc.org>
2022-12-05 23:02:19 -0800

    Day1Part1 of AOC

---
+++ aoc/Day1/day1.py
@@ -0,0 +1,25 @@
+# A yes, day 1, lets see how this goes :)
+
+# How many calories each elf is carrying
+# food is important
+Elfne   =   [1000, 2000, 3000]   # = 6000
+Elfwo   =   [4000]               # = 4000
+Elfree  =   [5000, 6000]         # = 11000
+Elfour  =   [7000, 8000, 9000]   # = 24000
+Elfive  =   [10000]              # = 10000
+
+sum(Elfne)
+sum(Elfwo)
+sum(Elfree)
+sum(Elfour)
+sum(Elfive)
+
+print("lol")
+# How I solved it lol
+# curl https://adventofcode.com/2022/day/1/input >> input.txt
+# vim, recorded macro (editing input.txt)
+#    0j bckspc +
+#$ sed 's/++/\n/g' test.txt >> workable.py
+# 0i print ( esc A ) esc 0 j i
+#$ python3 workable.txt >> numbers.py
+# 0ji esc bckspc , space

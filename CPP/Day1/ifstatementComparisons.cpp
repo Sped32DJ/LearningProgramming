d128e6bc71bab3a21e051829e453cb19a5a93fdf
Sped32DJ <dannycool10236@gmail.com>
2022-12-25 16:23:09 -0800

    Dec24

--- CPP/Day1/ifstatementComparisons.cpp
+++ CPP/Day1/ifstatementComparisons.cpp
@@ -6,12 +6,13 @@ int getMax(int num1, int num2, int num3) {
   int result;
 
   if (num1 >= num2 && num1 >= num3) {
-    result num1;
+    result = num1;
   } else if (num2 >= num1 && num2 >= num3) {
-    result num2;
+    result = num2;
   } else {
     result = num3;
   }
+  // There has to be a 1337 way to get the get the Max num
 
   /* if (num1 > num2) {
     result = num1;
@@ -22,7 +23,24 @@ int getMax(int num1, int num2, int num3) {
   return result;
 }
 
+int getMin(int one, int two, int three){
+
+  result Mini;
+}
 int main() {
-  cout << getMax(2, 5, 10) << endl;
+
+  int uno, dos, tres;
+  cout << "3 numbers to go into the array: ";
+  cin >> uno;
+  cin >> dos;
+  cin >> tres;
+  cout << "Endl\n\n" << endl;
+
+  cout << getMax(uno, dos, tres) << endl;
+  cout << getMax(1, 5, 6) << endl;
+  cout << "\nPrinting uno dos tres" << endl;
+  cout << uno << " " << dos << " " << tres
+       << endl; // There is a better way to do this -_-
+
   return 0;
 }

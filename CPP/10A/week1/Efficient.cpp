57d8159ccf0643a50563bd57f75640c959823260
Sped32DJ <dannycool10236@gmail.com>
2023-04-09 01:57:12 -0700

    commenting

--- CPP/10A/week1/Efficient.cpp
+++ CPP/10A/week1/Efficient.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
   int baseDigit, headDigit;
-  cin >> baseDigit >> headDigit;
+  cin >> baseDigit >> headDigit; // 0 1
 
   string topLine = string(5, ' ') + to_string(headDigit) + '\n';
   string secondLine = string(5, ' ') + to_string(headDigit) + to_string(headDigit) + '\n';
@@ -13,6 +13,16 @@ int main() {
                        string(5, to_string(baseDigit)[0]) + string(3, to_string(headDigit)[0]) + '\n';
   string bottomLine = string(5, ' ') + to_string(headDigit) + to_string(headDigit) + '\n' +
                       string(5, ' ') + to_string(headDigit) + '\n';
+
+/* Matches this output, line by line
+     1
+     11
+00000111
+000001111
+00000111
+     11
+     1
+ */
 
   cout << topLine << secondLine << middleLines << bottomLine;
   return 0;

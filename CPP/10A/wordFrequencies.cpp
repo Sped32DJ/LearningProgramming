2ad1285449cb7b1c33dc29129af4caaacf18134d
Sped32DJ <dannycool10236@gmail.com>
2023-05-18 02:32:17 -0700

    Why was this so hard? It is 2am, I have an 9am class :sob:

--- CPP/10A/wordFrequencies.cpp
+++ CPP/10A/wordFrequencies.cpp
@@ -3,38 +3,51 @@
 #include <vector>
 using namespace std;
 
-int FindWordFrequencies() {
-  int size;
-  string sentence;
-  vector<int> vlist;
-  vector<int> vFrequency(size);
+int findwordfrequencies() {
+  int numWords;
+  cin >> numWords;
 
-  cin >> size;
-  getline(cin, sentence);
+  vector<string> sentence(numWords);
+  vector<int> vfrequency(numWords, 1);
 
-  /* for (int i; i < size; ++i) {
+  for (int i = 0; i < numWords; ++i) {
+    cin >> sentence.at(i);
+  }
+
+  // fixes string, changes space for \n (may depricate this)
+  /* for (int i = 0; i < sentence.size(); ++i) {
+    if (isspace(sentence.at(i))) {
+      sentence.at(i) = '\n'; // replaces space for \n
+    }
   } */
 
-  // nested forloop, replace space for \n
-  for (int i = 0; i < sentence.size(); ++i) {
-    if (isspace(sentence.at(i))) {
-      sentence.at(i) = '\n';
-    }
-    for (int j = 0; j < sentence.size(); ++j) {
-      cout << " - "
+  for (int i = 0; i < numWords; ++i) {
+    if (sentence.at(i) != "") { // segment each words
+      for (int j = i + 1; j < numWords; ++j) {
+        if (sentence.at(j) == sentence.at(i)) {
+          ++vfrequency.at(i); // Every word has an index
+          ++vfrequency.at(j); // How does this work? idk -_-
+        }
+      }
+      cout << sentence.at(i) << " - " << vfrequency.at(i) << endl;
     }
   }
-  // TODO  Find a way make a count for each word
-  // ++vFrequency.at(i);
-  // add the " - vFrequency.at(i)"
-  //    Make sure vFrequency correalates to one word
-  // Read from the start of the index, until reaches '\n'
+  // Look at vector (vector works!)
+  /* for (int i = 0; i < vfrequency.size(); ++i) {
+    cout << vfrequency.at(i);
+  }
+  cout << endl; */
+  // todo  find a way make a count for each word
+  // ++vfrequency.at(i);
+  // add the " - vfrequency.at(i)"
+  //    make sure vfrequency index correalates to one word
+  // read from the start of the index, until reaches '\n'
+  //
 
   // check string, (this should probably be depricated)
-  cout << sentence << endl;
 }
 
 int main() {
-  FindWordFrequencies();
+  findwordfrequencies();
   return 0;
 }

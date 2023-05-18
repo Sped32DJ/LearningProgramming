82e97b31852447ff3bd741932e2ba9704e4f4215
Sped32DJ <dannycool10236@gmail.com>
2023-05-18 11:50:45 -0700

    Less scuffed

--- CPP/10A/wordFrequencies.cpp
+++ CPP/10A/wordFrequencies.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 using namespace std;
 
-int findwordfrequencies() {
+void findwordfrequencies() {
   int numWords;
   cin >> numWords;
 
   vector<string> sentence(numWords);
-  vector<int> vfrequency(numWords, 1);
+  vector<int> vfrequency(numWords, 0);
 
   for (int i = 0; i < numWords; ++i) {
     cin >> sentence.at(i);
@@ -23,15 +23,17 @@ int findwordfrequencies() {
 
   for (int i = 0; i < numWords; ++i) {
     if (sentence.at(i) != "") { // segment each words
-      for (int j = i + 1; j < numWords; ++j) {
+      for (int j = 0; j < numWords; ++j) {
         if (sentence.at(j) == sentence.at(i)) {
           ++vfrequency.at(i); // Every word has an index
-          ++vfrequency.at(j); // How does this work? idk -_-
+          // cout << i << " " << j << " "<<  sentence.at(j) << " " << sentence.at(i) << endl;
+          continue;
         }
       }
       cout << sentence.at(i) << " - " << vfrequency.at(i) << endl;
     }
   }
+}
   // Look at vector (vector works!)
   /* for (int i = 0; i < vfrequency.size(); ++i) {
     cout << vfrequency.at(i);

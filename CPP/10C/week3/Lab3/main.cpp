d4ab6fd0ca1d8a0a2cf022e2c178e3cfd7711b9f
Sped32DJ <dannycool10236@gmail.com>
2023-10-23 01:11:55 -0700

    QualityCode

--- CPP/10C/week3/Lab3/main.cpp
+++ CPP/10C/week3/Lab3/main.cpp
@@ -1,47 +1,74 @@
+#include <ctime>
 #include <iostream>
 #include <stdexcept>
 #include <vector>
 
 using namespace std;
 
-// NOTE  PSEUDO CODE, I don't think this works
-// TODO  Templates!!
-// Learn to write a templated class
-enum T {
-  string,
-  double,
-  int
+// Part One:
+template <typename T> unsigned min_index(const vector<T> &vals, unsigned index);
+
+template <typename T> void selection_sort(vector<T> &vals);
+
+template <typename T> T getElement(vector<T> vals, int index) {
+  return vals.at(index);
 }
 
-vector<char>
-createVector() {
-  int vecSize = rand() % 26;
-  char c = 'a';
-  vector<char> vals;
-  for (int i = 0; i < vecSize; i++) {
-    vals.push_back(c);
-    c++;
-  }
-  return vals;
-}
+vector<char> createVector();
 
-// WTF is a T datatype
-void selection_sort(vector<T> vals, int index) {}
-T getElement {}
-
+// Part B
 int main() {
-
-  // Part B
   srand(time(0));
   vector<char> vals = createVector();
   char curChar;
   int index;
   int numOfRuns = 10;
   while (--numOfRuns >= 0) {
-    cout << "Enter a number: " << endl;
-    cin >> index;
-    curChar = getElement(vals, index);
-    cout << "Element located at " << index << ": is " << curChar << endl;
+    try {
+      cout << "Enter a number: " << endl;
+      cin >> index;
+      if (cin.fail()) {
+        throw runtime_error("Bad input index");
+      }
+      curChar = getElement(vals, index); // .at() will throw an out_of_range
+      cout << "Element located at " << index << ": is " << curChar << endl;
+    } catch (const std::out_of_range &excpt) {
+      cout << excpt.what() << endl;
+      cout << "out of range exception occured" << endl;
+    }
   }
+
   return 0;
+}
+
+template <typename T>
+unsigned min_index(const vector<T> &vals, unsigned index) {
+  T minVal = vals.at(index); // Must initialize with index
+  unsigned minIn = index;
+
+  for (size_t i = index + 1; i < vals.size(); ++i) {
+    if (minVal > vals.at(i)) {
+      minVal = vals.at(i);
+      minIn = i;
+    }
+  }
+  return minIn;
+}
+
+template <typename T> void selection_sort(vector<T> &vals) {
+  for (size_t i = 0; i < vals.size(); ++i) {
+    unsigned minIn = min_index(vals, i);
+    swap(vals.at(i), vals.at(minIn)); // no need for nested loop
+  }
+}
+
+vector<char> createVector() {
+  int vecSize = rand() % 26;
+  char c = 'a';
+  vector<char> vals;
+  for (int i = 0; i < vecSize; ++i) {
+    vals.push_back(c);
+    ++c;
+  }
+  return vals;
 }

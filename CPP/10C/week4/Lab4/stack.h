50ad14a6774b6543443868a45d5aee4eacca890a
Sped32DJ <dannycool10236@gmail.com>
2023-11-01 14:32:47 -0700

    LIFO, A STACK IS LIFO

--- CPP/10C/week4/Lab4/stack.h
+++ CPP/10C/week4/Lab4/stack.h
@@ -12,20 +12,17 @@ private:
   static const int MAX_SIZE = 20;
   T *data;
   int size; // Represents accessible values
-  int front;
 
 public:
   // constructor
-  // Make sure to get T template
-  stack() : size(0), front(0) { data = new T[MAX_SIZE]; }
+  stack() : size(0) { data = new T[MAX_SIZE]; }
 
   // Parameterized constructor
-  stack(T val) : size(0), front(0) { push(val); }
+  stack(T val) : size(0) { push(val); }
 
   // copy constructor
   stack(const stack &cpy) {
     size = cpy.size;
-    front = cpy.front;
     std::copy(cpy.data, cpy.data + MAX_SIZE, data);
   }
 
@@ -35,25 +32,20 @@ public:
     size = 0;
   }
 
+  // Pushes to the top
   void push(T val) {
     if (isFull()) {
       throw overflow_error("Called push on full stack.");
     }
-    // Pushes to the front of the stack, then increments size
-    data[(front + size) % MAX_SIZE] = val;
-    ++size;
+    data[size++] = val;
   }
 
-  void pop() {
+  // Pops from the top
+  T pop() {
     if (empty()) {
       throw out_of_range("Called pop on empty stack.");
     }
-    if (isFull()) {
-      throw out_of_range("Reached max size");
-    }
-    top();
-    --size; // Value is still in memory, but is now trash
-            // Cheaper to "deallocate" then to overwrite
+    return data[--size]; // decrements size then returns value
   }
 
   void pop_two() {
@@ -68,9 +60,10 @@ public:
     if (empty()) {
       throw underflow_error("Called top on empty stack.");
     }
-    return data[(front + size - 1) % MAX_SIZE]; // TODO  Is this correct?
+    return data[size - 1];
   }
 
+  // two useful helper functions
   bool empty() const { return size == 0; }
   bool isFull() const { return size == MAX_SIZE; }
 };

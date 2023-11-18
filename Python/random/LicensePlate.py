7baf83640ecfa3e4e3df17d0fa4836af43604b28
Sped32DJ <dannycool10236@gmail.com>
2023-11-18 15:38:01 -0800

    Some python fun

---
+++ Python/random/LicensePlate.py
@@ -0,0 +1,10 @@
+plate = input("Plate: ")
+for char in plate:
+    print(char, char.isalpha())
+if plate[0].isalpha() && plate[1].isalpha:
+    if plate.size() > 2 && plate.size() < 6:
+        for char in plate:
+            if char == '.' || char == ' '
+            else:
+                break
+            if

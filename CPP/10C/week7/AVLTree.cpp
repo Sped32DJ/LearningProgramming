c515807d078a4306c7e7785ed3919a9f01c62698
Sped32DJ <dannycool10236@gmail.com>
2023-11-20 15:03:02 -0800

    Still broken

--- CPP/10C/week7/AVLTree.cpp
+++ CPP/10C/week7/AVLTree.cpp
@@ -155,42 +155,323 @@ void AVLTree::insert(const string &key, Node *curr) {
   }
 }
 
-/* void AVLTree::insert(const string &key, Node *curr) {
-  if (curr == nullptr) { // base case
+void AVLTree::remove(const string &key) { root = fix(root, key); }
+#include "AVLTree.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// TODO Deep copy
+/* AVLTree::AVLTree(AVLTree &cpy) {} */
+
+// TODO
+/* AVLTree::~AVLTree() {} */
+
+// AVL Stuff
+
+// Rotations
+void AVLTree::rotate(Node *curr) {
+  if (getBalance(curr) == 2) {
+    if (getBalance(curr->left) == -1)
+      rotateLeft(curr->left);
+    rotateLeft(curr->left);
+  } else if (getBalance(curr) == -2) {
+    if (getBalance(curr->right) == 1)
+      rotateRight(curr->right);
+    rotateLeft(curr);
+  }
+}
+
+void AVLTree::rotateLeft(Node *curr) {
+  // Store the necessary nodes for rotation
+  Node *RLC = curr->right->left; // Right Left Child, new root
+  if (curr->parent != nullptr) {
+    // Parent, parentchild, desiredchild
+    replaceChild(curr->parent, curr, curr->right); // See how to std::swap this
+  } else {
+    root = curr->right;
+    root->parent = nullptr;
+  }
+
+  setChild(curr->left, 'R', curr);
+  setChild(curr, 'L', RLC);
+}
+
+void AVLTree::rotateRight(Node *curr) {
+  // Store the necessary nodes for rotation
+  Node *LRC = curr->left->right; // Left Right Child, new root
+
+  // Update the parent of the current node's left child
+  if (curr->parent != nullptr) {
+    replaceChild(curr->parent, curr, curr->left);
+  } else {
+    root = curr->left;
+    root->parent = nullptr;
+  }
+
+  LRC = curr;
+  curr->left = LRC;
+  setChild(curr->left, 'R', curr);
+  setChild(curr, 'L', LRC);
+}
+
+void AVLTree::setChild(Node *parent, const char &direction, Node *child) {
+  if (direction != 'L' && direction != 'R') {
+    throw runtime_error("Wrong direction");
+  }
+  if (direction == 'L') {
+    parent->left = child;
+  } else {
+    parent->right = child;
+  }
+  if (child != nullptr) { // prevents segfaults
+    child->parent = parent;
+  }
+}
+void AVLTree::replaceChild(Node *parent, Node *parentchild,
+                           Node *desiredChild) {
+  if (parent->left == parentchild) {
+    setChild(parent, 'L', desiredChild);
+  } else if (parent->right == parentchild) {
+    setChild(parent, 'R', desiredChild);
+  } else {
+    cout << "Nah";
+  }
+}
+// Follows findDeepest logic
+int AVLTree::heightAtNode(Node *curr) const {
+  if (!root) {
+    return -1;
+  }
+  return max(heightAtNode(curr->left), heightAtNode(curr->right)) + 1;
+}
+
+void AVLTree::printBalanceFactors() { printBalanceFactors(root); }
+
+void AVLTree::printBalanceFactors(Node *curr) {
+  if (curr) { // prevents StackOverflow
+    // Recursively travels left then print
+    printBalanceFactors(curr->left);
+    cout << curr->data << '(' << getBalance(curr) << ')' << ", ";
+    // Recursively travels right
+    printBalanceFactors(curr->right);
+  }
+}
+
+void AVLTree::insert(const string &key) {
+  if (root == nullptr) { // base case
     Node *newNode = new Node(key);
-    root = newNode; // intializing for empty
+    root = newNode;
+  } else {
+    insert(key, root); // Calls helper
+  }
+}
+void AVLTree::insert(const string &key, Node *curr) {
+  if (curr == nullptr) {
+    // Base case: Create a new node and set it as the root if the tree is empty
+    Node *newNode = new Node(key);
+    root = newNode;
     return;
   }
 
-  // Right subtree
-  if (key > curr->data) {         // key is greater than curr
-    if (curr->right == nullptr) { // If null right child
-                                  // Place right
+  if (key > curr->data) {
+    if (curr->right == nullptr) {
+      // Insert the new node on the right and perform rotations
       Node *newNode = new Node(key);
       curr->right = newNode;
       newNode->parent = curr;
-      rotate(curr); // checks and performs rotations
+
+      // Perform rotations after inserting the node
+      while (curr) {
+        rotate(curr);
+        curr = curr->parent;
+      }
     } else {
-      // Rercusively traverses right
+      // Recursively insert on the right
       insert(key, curr->right);
     }
-    // Left subtree
-  } else if (key < curr->data) { // Key is lesser than curr
-    if (curr->left == nullptr) { // If left null
+  } else if (key < curr->data) {
+    if (curr->left == nullptr) {
+      // Insert the new node on the left and perform rotations
       Node *newNode = new Node(key);
       curr->left = newNode;
       newNode->parent = curr;
-      rotate(curr); // Checks and performs rotations
 
+      // Perform rotations after inserting the node
+      while (curr) {
+        rotate(curr);
+        curr = curr->parent;
+      }
     } else {
+      // Recursively insert on the left
       insert(key, curr->left);
     }
-  } else if (curr->data == key) {
+  } else {
+    // Key is equal to current node's data, increment count
     curr->incrementCount();
   }
-} */
+}
 
 void AVLTree::remove(const string &key) { root = fix(root, key); }
+
+Node *AVLTree::fix(Node *curr, const string &key) {
+  if (!curr)
+    return nullptr;
+  else if (curr->data == key) {
+    if (curr->count > 1) {
+      // If count is greater than 1, just decrement and return node
+      curr->decrementCount();
+      return curr;
+    }
+
+    if (curr->isLeaf()) { // if leaf
+      delete curr;        // Can be terminated, no issues, return null
+      return nullptr;
+    } else if (curr->onlyLeft()) { // Only left children
+      Node *victim = curr->left;
+      while (victim->right) {
+        victim = victim->right; // After this loop ->right is null
+      }
+
+      curr->cloneStats(victim); // Copies data/count
+      victim->count = 1;        // Flagged with 1 to be killed
+      curr->left = fix(curr->left, victim->data);
+      return curr;
+    } else if (curr->onlyRight()) { // Only right children
+      Node *victim = curr->right;
+      while (victim->left) {
+        victim = victim->left;
+      }
+      curr->cloneStats(victim);
+      victim->count = 1; //  Flags it with a count of 1 to be deleted in the
+                         //  recursive loop
+      curr->right = fix(curr->right, victim->data);
+      return curr;
+    } else { // Parents, have both right and left
+      Node *victim = curr->left;
+      while (victim->right) {
+        victim = victim->right;
+      }
+      curr->cloneStats(victim);
+      victim->count = 1; //  Flags it with a count of 1 to be deleted in the
+                         //  recursive loop
+      curr->left = fix(curr->left, victim->data);
+      return curr;
+    }
+  } else if (key < curr->data) {
+    // Key is less than curr node's data, go left
+    curr->left = fix(curr->left, key);
+    return curr;
+  } else {
+    // key is greater than curr's data, go right
+    curr->right = fix(curr->right, key);
+    return curr;
+  }
+}
+
+// END of AVL stuff
+
+void AVLTree::visualizeTree(const string &outputFilename) {
+  ofstream outFS(outputFilename.c_str());
+  if (!outFS.is_open()) {
+    cout << "Error" << endl;
+    return;
+  }
+  outFS << "digraph G {" << endl;
+  visualizeTree(outFS, root);
+  outFS << "}";
+  outFS.close();
+  string jpgFilename =
+      outputFilename.substr(0, outputFilename.size() - 4) + ".jpg";
+  string command = "dot -Tjpg " + outputFilename + " -o " + jpgFilename;
+  system(command.c_str());
+}
+
+void AVLTree::visualizeTree(ofstream &outFS, Node *n) {
+  if (n) {
+    if (n->left) {
+      visualizeTree(outFS, n->left);
+      outFS << n->data << " -> " << n->left->data << ";" << endl;
+    }
+
+    if (n->right) {
+      visualizeTree(outFS, n->right);
+      outFS << n->data << " -> " << n->right->data << ";" << endl;
+    }
+  }
+}
+
+// Find Extremes functions
+string AVLTree::largest() const {
+  if (isEmpty()) {
+    return "";
+  }
+  Node *curr = root;
+
+  if (curr->isLeaf()) {
+    return curr->data;
+  }
+  while (curr->right) {
+    curr = curr->right;
+  }
+  return curr->data;
+}
+
+string AVLTree::smallest() const {
+  if (isEmpty()) {
+    return "";
+  }
+  Node *curr = root;
+
+  if (curr->isLeaf()) {
+    return curr->data;
+  }
+
+  while (curr->left) {
+    curr = curr->left;
+  }
+  return curr->data;
+}
+
+// Printing tree using recursive helper functions
+void AVLTree::preOrder() const { preOrder(root); }
+void AVLTree::inOrder() const { inOrder(root); }
+void AVLTree::postOrder() const { postOrder(root); }
+// Visit root
+// Traverse left subtree
+// Traverse right subtree
+void AVLTree::preOrder(Node *curr) const {
+  if (curr) {
+    cout << curr->data << '(' << curr->count << "), ";
+    preOrder(curr->left);
+    preOrder(curr->right);
+  }
+}
+
+// Traverse left subtree
+// visit root
+// traverse the right subtree
+void AVLTree::inOrder(Node *curr) const {
+  if (curr) {
+    inOrder(curr->left);
+    cout << curr->data << '(' << curr->count << "), ";
+    inOrder(curr->right);
+  }
+}
+
+// Traverse left subtree
+// Traverse right subtree
+// visit root
+void AVLTree::postOrder(Node *curr) const {
+  if (curr) {
+    postOrder(curr->left);
+    postOrder(curr->right);
+    cout << curr->data << '(' << curr->count << "), ";
+  }
+}
+
+bool AVLTree::isEmpty() const { return !root; }
 
 Node *AVLTree::fix(Node *curr, const string &key) {
   if (!curr)

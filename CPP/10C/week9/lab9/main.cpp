0902e7c4d19d2d7364492ef0dc75388358905e00
Sped32DJ <dannycool10236@gmail.com>
2023-12-05 22:29:10 -0800

    Better storage

--- CPP/10C/week9/lab9/main.cpp
+++ CPP/10C/week9/lab9/main.cpp
@@ -3,18 +3,32 @@ using namespace std;
 
 void Quicksort_midpoint(
     int numbers[], int i,
-    int k); // this function sorts the given array in the range from i to k
-            // using quicksort method. In this function, pivot is the midpoint
-            // element (numbers[(i+k)/2]). (implementation of this function is
-            // given in section 21.5)
+    int k) // this function sorts the given array in the range from i to k
+           // using quicksort method. In this function, pivot is the midpoint
+           // element (numbers[(i+k)/2]). (implementation of this function is
+           // given in section 21.5)
+{
+  if (i < k) {
+    pi = partition(numbers[], low, high);
+
+    Quicksort_midpoint(numbers[], i, k - 1); // before partition
+    Quicksort_midpoint(numbers[], i + 1, k); // after partition
+  }
+}
+
+void partition(int arr[], int start, int end) {
+  int pivot = arr[start];
+  ;
+}
+
 void Quicksort_medianOfThree(
     int numbers[], int i,
     int k); // this function utilizes different pivot selection technique in
             // quicksort algorithm. The pivot is the median of the following
-            // three values: leftmost (numbers[i]), midpoint (numbers[(i+k)/2])
-            // and rightmost (numbers[k]). You should modify the partition
-            // function given in section 21.5 to select the pivot using
-            // median-of-three technique.
+            // three values: leftmost (numbers[i]), midpoint
+            // (numbers[(i+k)/2]) and rightmost (numbers[k]). You should
+            // modify the partition function given in section 21.5 to select
+            // the pivot using median-of-three technique.
 void InsertionSort(
     int numbers[],
     int numbersSize); // this function sorts the given array using InsertionSort
@@ -24,6 +38,7 @@ void InsertionSort(
 static const int NUMBERS_SIZE = 50000;
 
 int genRandInt(int low, int high) { return low + rand() % (high - low + 1); }
+
 void fillArrays(int arr1[], int arr2[], int arr3[]) {
   for (int i = 0; i < NUMBERS_SIZE; ++i) {
     arr1[i] = genRandInt(0, NUMBERS_SIZE);

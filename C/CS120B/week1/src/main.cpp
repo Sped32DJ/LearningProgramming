e7e8cfc0a4de553d2688ee57a23e6950898ebe1a
Sped32DJ <dannycool10236@gmail.com>
2024-10-01 11:16:35 -0700

    Kinda cooking here

--- C/CS120B/week1/src/main.cpp
+++ C/CS120B/week1/src/main.cpp
@@ -13,24 +13,39 @@ int main(int argc, char *argv[]) {
 
   printf("3rd arugment: %s\n", argv[2]);
 
-  printf("%s in binary: %s\n", argv[0], cptrchartobinary(argv[0]));
+  printf("%s in binary: %s\n", argv[0], cptrCharToBinary(argv[0]));
+  // Not sure if %s will dereference cptrCharToBinary
 
   printf("lower 4 bits of %s: %s\n", argv[0], ccharhexselector(argv[0]));
 
   printf("digit index %s of %s in hex is: %s\n", argv[3], argv[2],
          cinthexselector(atoi(argv[3]), argv[2]));
   return 0;
-}
+};
 
 char *cptrCharToBinary(const unsigned char val) {
   int n = atoi(val);
-  char *bin;
-  for (int i = 31; i >= 0; --i) {
-    printf("((n>>i)&1)\n"); // Shift right and btiwse AND with 1
-  };
-  return bin;
-}
+  char *bin; // Remember this is a pointer...
 
-char cCharHexSelector(const unsigned char hex, const unsigned char) {}
+  int i = 0;
+  // while n is not #0
+  while (~(n & 0x00)) {
+    // Adding to right of string
+    // Checking LMB
+    if (n & 0x80) {
+      bin += '1';
+    } else {
+      bin += '0';
+    }
+    // Popping off the top (LMB)
+    n << 1;
+    ++i;
+  }
+  return bin;
+};
+
+char cCharHexSelector(const unsigned char hex, const unsigned char hex) {};
+
+char cIntHexSelector(const unsigned int index, const unsigned char hex) {};
 
 // implement your functions here

a6a80fcf34d67c6d4d129b3e029dbdc581b87a47
Sped32DJ <dannycool10236@gmail.com>
2024-10-01 23:51:38 -0700

    Works perfectly... In my head

--- C/CS120B/week1/src/main.cpp
+++ C/CS120B/week1/src/main.cpp
@@ -9,6 +9,7 @@ char cIntHexSelector(const unsigned int, const unsigned char);
 
 // create unsigned char and unsigned int variables here
 int main(int argc, char *argv[]) {
+  // NOTE: Do I need to free any memory?
   printf("1st argument: %s\n", argv[0]);
 
   printf("3rd arugment: %s\n", argv[2]);
@@ -24,10 +25,13 @@ int main(int argc, char *argv[]) {
 };
 
 char *cptrCharToBinary(const unsigned char val) {
-  int n = atoi(val);
-  char *bin; // Remember this is a pointer...
+  /* int n = atoi(val); */
+  char *bin = (char *)malloc(sizeof(char[9])); // Remember this is a pointer...
+  if (!bin) {
+    return NULL;
+  }
 
-  // while n is not #0
+  /* // while n is not #0
   while (n != 0) {
     // Adding to right of string
     // Checking LMB
@@ -39,9 +43,20 @@ char *cptrCharToBinary(const unsigned char val) {
     }
     // Popping off the top (LMB)
     n << 1;
-  }
+  } */
 
-  bin += '\0';
+  // Start from the beginning
+  for (int i = 7; i >= 0; --i) {
+    bin[7 - i] = (val & (1 << i)) ? '1' : '0'; // Goes through each bit
+
+    // Ex: i = 1
+    // bin[6] = val & (1 << 2) ->
+    // bin[6] = val & 0x02
+    // if bit at that point returns true
+  }
+  bin[8] = '\0'; // Null ends the string
+
+  /* bin += '\0'; */
 
   return bin;
 };
@@ -49,15 +64,23 @@ char *cptrCharToBinary(const unsigned char val) {
 char cCharHexSelector(const unsigned char nibble, const unsigned char bit) {
   // if bit == 0; lower nibble
   // if bit == 1; upper nibble
-  // TODO: Return as hex instead int
 
-  return (bit) ? atoi(nibble & 0x0F) : atoi(nibble >> 4);
+  unsigned char resultNibble = (bit) ? nibble & 0x0F : (nibble >> 4) & 0x0F;
+  // If lesser than 10, you start at zero
+  // if greater than 10, you start at A, then remove 10 to account for the
+  // increase
+  return (resultNibble < 10) ? '0' + resultNibble : 'A' + (resultNibble - 10);
 };
 
 char cIntHexSelector(const unsigned int val, const unsigned char index) {
-  // TODO Convert the int
-  // Make sure it outputs as a char
-  return (val >> (index * 4));
-};
+  // Extract nibble at given index
+  unsigned int nibble = (val >> (index * 4)) &
+                        0x0F; // Shifts it all the way to the right
+                              // Then masks to isolate the Right Most Nibble
 
-// implement your functions here
+  // Convert nibble into hex
+  // if lesser than 10, output nibble literal
+  // greater, start at 10 and set back the nibble by 10
+  //    Then do char multiplication
+  return (nibble < 10) ? '0' + nibble : 'A' + (nibble - 10);
+};

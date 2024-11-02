a9fee0e288de53058318115cd6aa0741057f3ba9
Sped32DJ <dannycool10236@gmail.com>
2024-11-01 19:11:32 -0700

    Good progress, LEDs just not ligting up

--- C/CS120B/week5/src/main.cpp
+++ C/CS120B/week5/src/main.cpp
@@ -6,7 +6,7 @@
 // Exercise 1: 3 tasks
 // Exercise 2: 5 tasks
 // Exercise 3: 7 tasks
-#define NUM_TASKS 3
+#define NUM_TASKS 5
 
 // Task struct for concurrent synchSMs implmentations
 typedef struct _task {
@@ -22,6 +22,21 @@ const unsigned long GCD_PERIOD = 1;
 const unsigned long SNAR_PERIOD = 1000;
 const unsigned long DISP_PERIOD = 1;
 const unsigned long LEFT_PERIOD = 200;
+
+const unsigned long RED_PERIOD = 1;
+const unsigned long RED_PWN = 10;
+
+const unsigned long GREEN_PERIOD = 1;
+const unsigned long GREEN_PWN = 10;
+
+const unsigned int threhold_close = 8;
+const unsigned int threhold_far = 12;
+
+unsigned char RedH = 0;
+unsigned char RedL = 0;
+
+unsigned char GreenH = 0;
+unsigned char GreenL = 0;
 task tasks[NUM_TASKS]; // declared task array with NUM_TASKS amount of tasks
 
 // TODO: Define, for each task:
@@ -29,14 +44,17 @@ task tasks[NUM_TASKS]; // declared task array with NUM_TASKS amount of tasks
 //  (2) tick functions
 enum SNAR_States { SonarInit, SONAR_S1 };
 volatile double distance = 0;
-unsigned char in_distance = 16;
-unsigned char cm_distance = 22;
+unsigned char in_distance = 69;
+unsigned char cm_distance = 169;
 
 enum DISP_States { displayInit, disp_S1, disp_S2, disp_S3, disp_S4 };
 bool isInches = false; // default metric (yucky); true = imperial
 
 enum Left_States { leftInit, Left_S1, Left_S2 };
 unsigned char LeftButton() { return !GetBit(PINC, 1); }
+
+enum Red_States { RedInit, RPwmH, RPwmL };
+enum Green_States { GreenInit, GPwmH, GPwmL };
 
 // Tick Functions
 int sonar_TickFct(int state) {
@@ -60,8 +78,30 @@ int sonar_TickFct(int state) {
     distance = sonar_read();
     in_distance = int(distance / 2.54);
     cm_distance = int(distance);
+
     // NOTE: Hopefully this works soon
     // serial_println(in_distance);
+    if (cm_distance < threhold_close) {
+      RedH = 10;
+      RedL = 0;
+
+      GreenH = 0;
+      GreenL = 10;
+
+    } else if (cm_distance < threhold_far) {
+      RedH = 9;
+      RedL = 1;
+
+      GreenH = 3;
+      GreenL = 7;
+    } else {
+      RedH = 0;
+      RedL = 10;
+
+      GreenH = 10;
+      GreenL = 0;
+    }
+
     break;
   }
   return state;
@@ -122,7 +162,6 @@ int display_TickFct(int state) {
 }
 
 // Only two states, should technically work perfectly
-// NOTE: If fucked, make 4 states
 int left_TckFct(int state) {
   // Transitions
   switch (state) {
@@ -158,6 +197,122 @@ int left_TckFct(int state) {
   return state;
 }
 
+int red_TckFct(int state) {
+  unsigned int i = 0;
+  // Transitions
+  switch (state) {
+  case RedInit:
+    if (cm_distance < threhold_close) {
+      RedH = 10;
+      RedL = 0;
+    } else if (cm_distance < threhold_far) {
+      RedH = 9;
+      RedL = 1;
+    } else {
+      RedH = 0;
+      RedL = 0;
+    }
+    break;
+  case RPwmH:
+    if (i < RedH) {
+      state = RPwmH;
+    } else {
+      state = RPwmL;
+      i = 0;
+    }
+    break;
+  case RPwmL:
+    if (i < RedL) {
+      state = RPwmL;
+    } else {
+      state = RPwmH;
+      i = 0;
+    }
+    break;
+  default:
+    state = RedInit;
+    break;
+  }
+
+  // State Actions
+  switch (state) {
+  case RedInit:
+    PORTC &= 0xF7; // Turn off Red LED
+    ++i;
+    break;
+  case RPwmH:
+    PORTC |= 0x08; // Turn on Red LED
+    ++i;
+    break;
+  case RPwmL:
+    PORTC &= 0xF7; // Turn off Red LED
+    ++i;
+    break;
+  default:
+    break;
+  }
+  return state;
+}
+int green_TckFct(int state) {
+  unsigned int i = 0;
+  // Transitions
+  switch (state) {
+  case GreenInit:
+    if (cm_distance < threhold_close) {
+      GreenH = 0;
+      GreenL = 10;
+      // 0% Duty
+    } else if (cm_distance < threhold_far) {
+      GreenH = 3;
+      GreenL = 7;
+      // 30% Duty
+    } else {
+      GreenH = 10;
+      GreenL = 0;
+      // 100% Duty
+    }
+    i = 0;
+    break;
+  case GPwmH:
+    if (i < GreenH) {
+      state = GPwmH;
+    } else {
+      state = GPwmL;
+      i = 0;
+    }
+    break;
+  case GPwmL:
+    if (i < GreenL) {
+      state = GPwmL;
+    } else {
+      state = GPwmH;
+      i = 0;
+    }
+    break;
+  default:
+    state = GreenInit;
+    break;
+  }
+
+  // State Actions
+  switch (state) {
+  case GreenInit:
+    PORTC &= 0xEF; // Turn off green LED
+    break;
+  case GPwmL:
+    // Turn off green LED
+    PORTC &= 0xEF;
+    ++i;
+    break;
+  case GPwmH:
+    PORTC |= 0x10; // Turn on Green LED
+    ++i;
+    break;
+  }
+
+  return state;
+}
+
 void TimerISR() {
   for (unsigned int i = 0; i < NUM_TASKS; i++) {
     // Iterate through each task in the task array
@@ -186,16 +341,6 @@ int main(void) {
   sonar_init(); // initializes sonar
                 // serial_init(9600); // Helps debugging
 
-  // TODO: Initialize tasks here
-  //  e.g. tasks[0].period = TASK1_PERIOD
-  //  tasks[0].state = ...
-  //  tasks[0].elapsedTime = ...
-  //  tasks[0].TickFct = &task1_tick_function;
-  //    task tasks[]{{SonarInit, SNAR_PERIOD, tasks[0].period, &sonar_TickFct},
-  //               {displayInit, DISP_PERIOD, tasks[1].period,
-  //               &display_TickFct},
-  //             {leftInit, LEFT_PERIOD, tasks[2].period, &left_TckFct}};
-
   tasks[0].state = SonarInit;
   tasks[0].period = SNAR_PERIOD;
   tasks[0].elapsedTime = tasks[0].period;
@@ -210,6 +355,16 @@ int main(void) {
   tasks[2].period = LEFT_PERIOD;
   tasks[2].elapsedTime = tasks[2].period;
   tasks[2].TickFct = &left_TckFct;
+
+  tasks[3].state = RedInit;
+  tasks[3].period = RED_PERIOD;
+  tasks[3].elapsedTime = tasks[3].period;
+  tasks[3].TickFct = &red_TckFct;
+
+  tasks[4].state = GreenInit;
+  tasks[4].period = GREEN_PERIOD;
+  tasks[4].elapsedTime = tasks[4].period;
+  tasks[4].TickFct = &green_TckFct;
 
   TimerSet(GCD_PERIOD);
   TimerOn();

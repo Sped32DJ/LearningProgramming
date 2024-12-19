7337ae0a25b21493868e3c5507ab5e34fbb0ad56
Sped32DJ <dannycool10236@gmail.com>
2024-12-19 14:28:43 -0800

    encodeDecode working

--- CPP/leetcode/hashmaps/encodeDecode.cpp
+++ CPP/leetcode/hashmaps/encodeDecode.cpp
@@ -2,42 +2,33 @@
 #include <vector>
 using namespace std;
 
-// Current Status: It Brokey
 class Solution {
 public:
   string encode(vector<string> &strs) {
-    if (strs.empty()) {
-      return "";
-    }
     string result;
 
     for (auto &s : strs) {
       // result += (s.size() + '0') + s;
-      result += static_cast<char>(s.size()) + s;
+      result += to_string(s.size()) + '#' + s;
     }
 
     return result;
   }
 
-  vector<string> decode(string s) {
+  vector<string> decode(const string &s) {
     vector<string> result;
-    if (s.empty()) {
-      return result;
-    }
+    size_t i = 0;
 
-    string buffer;
-    for (auto c : s) {
-      // checks if c is a number
-      if (c >= '0' && c <= '9') {
-        // Empty, doesn't push into the result vector
-        if (!buffer.empty())
-          result.push_back(buffer);
-        // Clears buffer since it has been presumably pushed back
-        buffer.clear();
-      } else {
-        // Adds to buffer if it detects a character
-        buffer.push_back(c);
-      }
+    while (i < s.size()) {
+      size_t delimiter = s.find('#', i);
+      if (delimiter == string::npos)
+        break; // Shouldn't happen if input is valid
+
+      int length = stoi(s.substr(i, delimiter - i)); // Get size prefix
+      string str = s.substr(delimiter + 1, length);  // Extract string
+
+      result.push_back(str);
+      i = delimiter + 1 + length; // Move to the next part of the encoded string
     }
 
     return result;

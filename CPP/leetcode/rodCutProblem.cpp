ca14a8103a8f62618db8d014024672b0b7f46a36
Sped32DJ <dannycool10236@gmail.com>
2025-02-20 10:04:52 -0800

    rodcut problem finished, recursive and iterative

--- CPP/leetcode/rodCutProblem.cpp
+++ CPP/leetcode/rodCutProblem.cpp
@@ -4,42 +4,77 @@ using namespace std;
 
 class Solution {
 public:
-  int rodCutTopDown(int price[], int n) {
+  int rodCutTopDownRef(int price[], int n) {
     int val[n + 1];
     val[0] = 0;
     int i, j;
     for (i = 1; i <= n; i++) {
-      int max_val = INT_MIN;
-      for (j = 0; j < i; j++)
+      int max_val = 0;
+      for (j = 1; j < i; j++)
         max_val = max(max_val, price[j] + val[i - j - 1]);
       val[i] = max_val;
     }
     return val[n];
   }
 
-  int rodCutRecurr(vector<int> &price) {
-    int n = price.size();
-
-    return rodCutRecurr(price, n); // What is the price of length n?
-  }
-  int rodCutHelper(vector<int> &price, int i) {
-    // Base case
-    if (n == 0)
-      return 0;
-
-    int maxVal = 0; // -infty
-
-    for (int j = 1; j < i; ++j) {
-      // We decide that our maxVal is equal to the curr Max, the price at j
-      // + the remaining rod
-      maxVal = max(maxVal, price[j] + rodCutHelper(price, i - j - 1));
+  int rodCutTopDownHelp(vector<int> r, int n) {
+    // Initialize r[] with trash values
+    for (int i = 0; i < n; i++) {
+      r[i] = 0;
     }
-    return maxVal;
+    return rodCutTopDown(r, n); // Asks for the value for rod length n
+  }
 
-    // int val[n + 1];
-    // val[0] = 0;
-    // for (int i = 1; i <= n; i++)
-    //   val[i] = INT_MIN;
-    // return rodCutRecurr(price, n);
+  int rodCutTopDown(vector<int> r, int i) {
+    // Base case
+    if (i == 0)
+      return 0;
+    // If the value is already calculated, return it
+    if (r[i] > 0)
+      return r[i];
+    int q = 0; // Holds max
+
+    for (int j = 1; j < i; j++) {
+      // It is either the current max or the new max
+      // It knows the value of r[j] but asks for the value of [i-j]
+      q = max(q, r[j] + rodCutTopDown(r, i - j));
+    }
+    r[i] = q; // Updates the DP table with the max value
+    return q; // Returns the max value
+  }
+  int rodCutBottomUpRef(int price[], int n) {
+    int val[n + 1];
+    val[0] = 0;
+    int i, j;
+    for (i = 1; i <= n; i++) {
+      int max_val = 0;
+      for (j = 1; j < i; j++)
+        max_val = max(max_val, price[j] + val[i - j - 1]);
+      val[i] = max_val;
+    }
+    return val[n];
+  }
+
+  int rodCutBottomUp(int price[], int n) {
+    // Initiliaze dp table r[] with 0
+    int r[n + 1];
+    for (int i = 0; i < n; ++i) {
+      r[i] = 0;
+    }
+
+    // Bottom up approach
+    // Two for loops to check every solution possible
+    // The difference is that we have a DP table to store past results
+    for (int i = 1; i < n; ++i) {
+      // Holds the max
+      int q = 0;
+      for (int j = 1; j < i; ++j) {
+        // Updates the max
+        q = max(q, price[j] + r[i - j]);
+      }
+      r[i] = q;
+    }
+
+    return r[n];
   }
 };

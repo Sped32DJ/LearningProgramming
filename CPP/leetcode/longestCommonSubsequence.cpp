82a2bef475ee67e85446249ef35c707dd3b638f5
Sped32DJ <dannycool10236@gmail.com>
2025-02-20 17:03:52 -0800

    LCS easy

--- CPP/leetcode/longestCommonSubsequence.cpp
+++ CPP/leetcode/longestCommonSubsequence.cpp
@@ -3,27 +3,21 @@ using namespace std;
 
 class Solution {
 public:
-  int LCS(string s1, string s2) {
-    int q = 0; // This holds the max
-    int dp[s1.length() + 1][s2.length() + 1];
-    // This holds the length of the LCS
-    int bk[s1.length() + 1][s2.length() + 1]; // This helps reconstruct the LCS
+  int LCS(string s1, string s2, int m, int n) {
+    // Base case, either string is empty
+    if (m == 0 || n == 0)
+      return 0;
 
-    // Initialize the dp and bk arrays
-    for (int i = s1.length(); i < s1.length(); ++i) {
-      for (int j = s2.length(); j < s2.length(); ++j) {
-        dp[i][j] = 0;
-        bk[i][j] = 0;
-      }
+    // Last chars match
+    if (s1.at(m - 1) == s2.at(n - 1)) {
+      // So we cut off one from both strings and add 1 to the result
+      // We add 1 to the answer and ask if there is any more similarities left
+      // in the string
+      // All until the base case of either string being empty
+      return 1 + LCS(s1, s2, m - 1, n - 1);
+    } else {
+      // Then you take the max if you remove 1 from left string or right string
+      return max(LCS(s1, s2, m - 1, n), LCS(s1, s2, m, n - 1));
     }
-
-    for (int i = 0; i < s1.length(); ++i) {
-      for (int j = 0; j < s2.length(); ++j) {
-        q += (s1[i] == s2[j]) ? 1 : 0;
-        dp[i][j] = q;
-      }
-    }
-
-    return q;
   }
 };

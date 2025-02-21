79ce914f46fa699d5d40153055f54b7dc3d691af
Sped32DJ <dannycool10236@gmail.com>
2025-02-20 17:11:46 -0800

    Added DP and naive LCS

--- CPP/leetcode/longestCommonSubsequence.cpp
+++ CPP/leetcode/longestCommonSubsequence.cpp
@@ -1,9 +1,10 @@
 #include <string>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-  int LCS(string s1, string s2, int m, int n) {
+  int LCSNaive(string s1, string s2, int m, int n) {
     // Base case, either string is empty
     if (m == 0 || n == 0)
       return 0;
@@ -14,10 +15,33 @@ public:
       // We add 1 to the answer and ask if there is any more similarities left
       // in the string
       // All until the base case of either string being empty
-      return 1 + LCS(s1, s2, m - 1, n - 1);
+      return 1 + LCSNaive(s1, s2, m - 1, n - 1);
     } else {
-      // Then you take the max if you remove 1 from left string or right string
-      return max(LCS(s1, s2, m - 1, n), LCS(s1, s2, m, n - 1));
+      // Check 2 case
+      // 1. Exclude last char of s1
+      // 2. Excluse last char of s2
+      // Then keep the highest value
+      return max(LCSNaive(s1, s2, m - 1, n), LCSNaive(s1, s2, m, n - 1));
     }
+  }
+  int LCS(string s1, string s2, int m, int n, vector<vector<int>> &memo) {
+    // Base case; either string empty
+    if (m == 0 || n == 0)
+      return 0;
+
+    // We already know the answer
+    // No need to recalculate
+    if (memo[m][n] > 0)
+      return memo[m][n];
+
+    // The last two chars do match!
+    if (s1[m - 1] == s2[n - 1])
+      return 1 + LCS(s1, s2, m - 1, n - 1, memo);
+
+    // Last resort, 2 cases
+    // pop last char from string 1
+    // pop last char from string 2
+    return memo[m][n] =
+               max(LCS(s1, s2, m - 1, n, memo), LCS(s1, s2, m, n - 1, memo));
   }
 };
